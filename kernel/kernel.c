d4605d2901bc29bb85ab535b228975e53291fd3b
Fabrice <schaubfabrice@gmail.com>
2025-03-08 08:48:28 +0100

    atleast it boots

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -72,7 +72,7 @@ void uart_puts(const char *str) {
 }
 
 void kernel_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3) {
-    uart_init();
+   // uart_init();
     uart_puts("Hello, kernel World!\r\n");
     while (1)
         uart_putc(uart_getc());
---
+++ prepare.sh
@@ -0,0 +1,4 @@
+sudo mount /dev/sda1 /mnt
+sudo rm -rf /mnt/*
+sudo cp -r boot/* /mnt
+sudo umount /mnt
